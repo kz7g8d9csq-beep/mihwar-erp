e4d9ee01f9b34c98cc8f5a5db70db9502187064d
Ahmed Abdullah Helmy <ahmedhelmy82463@gmail.com>
2026-09-10 19:31:24 +0300

    Update correct Render API base URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://RENDER_BACKEND_URL_HERE.onrender.com'
+  baseURL: 'https://mihwar-backend.onrender.com'
 });
 
 export default API;
